a9aee30604bcbd31797e980d34a9296fc367dbae
su <wkdlfflxh@naver.com>
2021-11-29 21:34:40 +0900

    Feat : cookie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,53 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
+import './App.css';
+import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const MiniMain = () => {
+  const handleCookie = () => {
+    axios
+      .get('http://betti.kr:9003/api/cookie', {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log('성공!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleCookie2 = () => {
+    axios
+      .get('https://betti.kr:9000/api/cookie', {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log('성공!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-    We Can See MainCookie!
+      <button type="button" onClick={handleCookie} className="loginbutton">
+        cookie
+      </button>
+      <button type="button" onClick={handleCookie2} className="loginbutton">
+        cookie2
+      </button>
     </>
-  )
-}
+  );
+};
 
 const Main = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [isRemember, setIsRemember] = useState(false);
-  const [cookies, setCookie, removeCookie] = useCookies(["rememberText"]);
+  const [cookies, setCookie, removeCookie] = useCookies(['rememberText']);
 
   let now = new Date();
   let after1m = new Date();
@@ -37,45 +69,40 @@ const Main = () => {
     setIsRemember(e.target.checked);
     if (e.target.checked) {
       console.log('we save cookies on path "Main"');
-      setCookie("CookieKey", text, { path: "/Main", expires: after1m });
+      setCookie('CookieKey', text, { path: '/Main', expires: after1m });
     } else {
-      removeCookie("CookieKey");
+      removeCookie('CookieKey');
     }
   };
   return (
     <>
-  <input value={text} onChange={onChange}/>
-  <input type="checkBox" onChange={handleOnChange} checked={isRemember}/>
-    <h1>{text}</h1>
+      <input value={text} onChange={onChange} />
+      <input type="checkBox" onChange={handleOnChange} checked={isRemember} />
+      <h1>{text}</h1>
     </>
-  )
-}
+  );
+};
 
 const Cookie = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(["rememberText"]);
+  const [cookies, setCookie, removeCookie] = useCookies(['rememberText']);
 
   useEffect(() => {
-    console.log("try Find CookieKey")
+    console.log('try Find CookieKey');
     console.log(cookies);
-  }, [])
-  return(
-  <>
-  </>
-  )
-}
+  }, []);
+  return <></>;
+};
 function App() {
-  
   return (
     <>
       <Router>
-      <Routes>
-        <Route exact path="/Main" element={<Main/>} />
-        <Route exact path="/Main/main" element={<MiniMain/>} />
+        <Routes>
+          <Route exact path="/Main" element={<Main />} />
+          <Route exact path="/Main/main" element={<MiniMain />} />
 
-        <Route exact path="/Cookie" element={<Cookie/>} />
-      </Routes>
-    </Router>
-
+          <Route exact path="/Cookie" element={<Cookie />} />
+        </Routes>
+      </Router>
     </>
   );
 }
