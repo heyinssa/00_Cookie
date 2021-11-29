69d55f5d48348acf700e9f9738db5094953df81a
chichoon <chichoon.choi@gmail.com>
2021-11-29 14:47:15 +0900

    feat: app.js 업데이트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 
 function App() {
+  const [text, setText] = useState("");
+  const [isRemember, setIsRemember] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies(["rememberText"]);
+
+  let now = new Date();
+  let after1m = new Date();
+
+  useEffect(() => {
+    if (cookies.rememberText !== undefined) {
+      setText(cookies.rememberText);
+      setIsRemember(true);
+    }
+  }, []);
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  const handleOnChange = (e) => {
+    after1m.setMinutes(now.getMinutes() + 1);
+    setIsRemember(e.target.checked);
+    if (e.target.chekced) {
+      setCookie("rememberText", text, { path: "/", expires: after1m });
+    } else {
+      removeCookie("rememberText");
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -13,8 +42,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
