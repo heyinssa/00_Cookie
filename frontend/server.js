2134b44a8df5787a9b933e62b503c6ff68855aec
Sangjoon Ham <gulsamcono@gmail.com>
2021-11-30 11:36:36 +0900

    Feat : 프론트 JWT 백과 통신

---
+++ frontend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.set("port", port); // 서버의 포트에 인자(port)를 설정. 일종의 전역변수.
+
+app.post("/data", (req, res) => {
+  // 경로의 파일을 전송한다.
+  res.send("scccess");
+});
+
+// app.get("/data/special", (req, res) => {
+//   res.send(`특별한 데이터`);
+// });
+// app.get("/data/:input", (req, res) => {
+//   // 와일드 카드는 맨 아래에!
+//   res.send(`입력한 파라미터 값은 ${req.params.input}입니다.`);
+// });
+
+// app.get("/mypage", (req, res) => {
+//   res.send("success!");
+//   console.log("main 페이지");
+// });
+// app.get("/api/piscine", (req, res) => {
+//   res.send(["qwe", "zcxv", "asdg", "sa", "saf", "asf", "qwerwqrtwq"]);
+//   console.log("api 전송");
+// });
+app.listen(app.get("port"), () => {
+  console.log(`${port} 포트에서 동작중!`);
+}); 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios"
 
-const MiniMain = () => {
-  return <>We Can See MainCookie !</>;
-};
+const Login = () => {
+  const handlerClickButton = async() => {
+    try {
+      const data = {id : "kim", email:"kim@gmail.com"};
+      const URL = "http://localhost:5000/data"
+      console.log(data);
+      console.log(JSON.stringify(data));
+      const result = await axios.post(URL, JSON.stringify(data), {
+        headers: {
+          "Content-Type": `application/json`,
+        }
+      })
+      console.log(result.data);
+    }
+    catch (e) {
+      alert(e);
+    }
+  }
+  return (
+    <>
+    <h1>kim으로 로그인 시도합니다.</h1>
+    <button onClick={handlerClickButton} type="button">로그인</button>
+    </>
+  )
+}
 
 const Main = () => {
   const [text, setText] = useState("");
@@ -47,24 +69,14 @@ const Main = () => {
   );
 };
 
-const Cookie = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(["rememberText"]);
-
-  useEffect(() => {
-    console.log("try Find CookieKey");
-    console.log(cookies);
-  }, []);
-  return <></>;
-};
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/Main" element={<Main />} />
-          <Route exact path="/Main/main" element={<MiniMain />} />
+          <Route exact path="/main" element={<Main />} />
 
-          <Route exact path="/Cookie" element={<Cookie />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
