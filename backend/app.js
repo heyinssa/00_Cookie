3c67d15b2a678b9bbf679cbcce6e38d6acdde44b
dycha <petercha2000@gmail.com>
2021-12-03 09:08:26 +0000

    Feat : backend fixed

--- backend/app.js
+++ backend/app.js
@@ -7,33 +7,34 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	origin: true,
+	credentials: true
+}));
 app.use(morgan('combined'));
 app.use(helmet());
 app.use(cookieParser());
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Credentials', true);
+// 	res.header('Access-Control-Allow-Origin', req.headers.origin);
+// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+// 	res.header('Access-Control-Expose-Headers', "Set-Cookie")
+// 	next();
+//   });
 
-app.use('/api/cookie', function (req, res, next) {
-  console.log('here');
-  res.cookie('key', 'value', {
-    maxAge: 10000,
-    SameSite: 'none',
-    secure: true,
-  });
-  return res.send('cookie create');
+app.get('/api/v1/create', function (req, res, next) {
+	var betti = "Betti";
+	// expires: new Date(Date.now() + 9000000),
+	res.cookie('visitors_no_permanent', "1" , {
+		maxAge: 1000 * 60 * 60,
+		sameSite: "none",
+		secure: true,
+	});
+	console.log(req.cookies);
+
+	return res.send('Visitors created! asdf');
 });
 
-// app.get('/currentcounter', function(req, res) {
-//     var visitors = req.cookies.visitors;
-//     res.send('Visitors: ' + visitors);
-// });
 
-// app.get('/updatevisitors', function(req, res) {
-//     var new_value = 38;
-
-//     res.cookie('visitors', new_value);
-//     res.send('Visitors updated.');
-// });
-
-app.listen(9905, '0.0.0.0', () => {
-  console.log('Start!');
-});
+app.listen(5000, '0.0.0.0')
